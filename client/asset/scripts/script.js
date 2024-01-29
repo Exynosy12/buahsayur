ea3de86969d1b280253aba042722cc272d6e6450
Exynosy12 <107163901+Exynosy12@users.noreply.github.com>
2024-01-29 15:11:55 +0700

    Update script.js

--- client/asset/scripts/script.js
+++ client/asset/scripts/script.js
@@ -96,9 +96,9 @@ var checkoutAction = async () => {
         payable = payable + parseFloat(product.payable);
         items.push({
             id: parseInt(product.id),
-            name: product.name,
+            name: unit: Number.isInteger(product.unit) ? product.name : product.name + " " + parseInt(product.taken) + " KG",
             price: parseInt(product.price),
-            unit: parseInt(product.unit),
+            unit: Number.isInteger(product.unit) ? parseInt(product.unit) : 1,
             taken: parseInt(product.taken),
             payable: parseInt(product.payable)
         });
